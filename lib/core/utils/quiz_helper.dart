b2dc440e839f59c0aebae8a62df1265af921025f
Medalisekri <sekrimedali@gmail.com>
2026-07-31 15:13:57 +0100

    Add quiz question model and quiz helper utils

---
+++ lib/core/utils/quiz_helper.dart
@@ -0,0 +1,21 @@
+import 'dart:math';
+
+import 'package:countries_api/models/country.dart';
+import 'package:countries_api/models/quiz_questions.dart';
+
+List<QuizQuestions> generateQuestions(List<Country> countries){
+  final List<QuizQuestions> questions = [];
+  countries.shuffle();
+  final selected = countries.take(10).toList();
+for(final correct in selected) {
+ final other =  countries.where((c)=>c.name !=correct.name).toList();
+  other.shuffle();
+  final wrongOptions = other.take(3).toList();
+  final options = [correct.name ,...wrongOptions.map((c) => c.name)]..shuffle();
+ questions.add(QuizQuestions(
+     questions: correct.flagUrl,
+     options: options,
+     correctAnswer: correct.name));
+}
+return questions;
+}
---
+++ lib/models/quiz_questions.dart
@@ -0,0 +1,11 @@
+class QuizQuestions {
+  final String questions;
+  final List<String> options;
+  final String correctAnswer;
+
+  QuizQuestions({
+    required this.questions,
+    required this.options,
+    required this.correctAnswer
+});
+}
